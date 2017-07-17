fb3da2dc839a7d0e6343ac52fa4773403ae09707
heilmann <heilmann@mindscreen.de>
2017-07-17 14:21:03 +0200

    Use response interface for HTTP output

--- Classes/Controller/ReportsController.php
+++ Classes/Controller/ReportsController.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace Mindscreen\JsonReports\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\Stream;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class ReportsController
  */
-class ReportsController {
+class ReportsController
+{
 
     /**
      * Main method, used as eID script
@@ -19,28 +22,45 @@ class ReportsController {
      */
     public function indexAction(ServerRequestInterface $request, ResponseInterface $response)
     {
+        $responseBody = fopen('php://memory', 'r+');
+        $statusCode = 200;
+        $statusMessage = 'All reports OK';
+
         // Check if current IP address is in range
-        if (!GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'), $GLOBALS['EXTCONF']['json_reports']['allowedIpAddresses'])) {
-            echo 'Access denied for IP ' . GeneralUtility::getIndpEnv('REMOTE_ADDR');
-            return $response->withStatus('403');
-        }
+        if (!GeneralUtility::cmpIP(GeneralUtility::getIndpEnv('REMOTE_ADDR'),
+            $GLOBALS['EXTCONF']['json_reports']['allowedIpAddresses'])
+        ) {
+            $statusCode = 403;
+            $statusMessage = 'Access denied';
+            fwrite($responseBody, json_encode(['error' => 'Access denied']));
+        } else {
+            // execute CLI script
+            $reportsJson = exec('TYPO3_CONTEXT=' . \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->__toString() . ' ' . PATH_site . 'typo3/cli_dispatch.phpsh extbase reports:list');
 
-        // execute CLI script
-        $reportsJson = exec('TYPO3_CONTEXT=' . \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->__toString() . ' '  . PATH_site . 'typo3/cli_dispatch.phpsh extbase reports:list');
-        echo $reportsJson;
-
-        $reports = json_decode($reportsJson, true);
-
-        if (!is_array($reports)) {
-            return $response->withStatus('500', 'Reports could not be generated.');
-        }
-
-        foreach ($reports as $reportCategory)  {
-            foreach ($reportCategory as $status) {
-                if ($status['severity'] > 0) {
-                    return $response->withStatus('500', 'Reports contain warnings or errors.');
+            $reports = json_decode($reportsJson, true);
+            if (!is_array($reports)) {
+                fwrite($responseBody, json_encode(['error' => $reportsJson]));
+                $statusCode = 500;
+                $statusMessage = 'Reports could not be generated';
+            } else {
+                fwrite($responseBody, $reportsJson);
+                foreach ($reports as $reportCategory) {
+                    foreach ($reportCategory as $status) {
+                        if ($status['severity'] > 0) {
+                            $statusCode = 500;
+                            $statusMessage = 'Reports contain warnings or errors';
+                            break 2;
+                        }
+                    }
                 }
             }
         }
+
+        rewind($responseBody);
+        return $response
+            ->withHeader('Content-type', 'application/json')
+            ->withBody(new Stream($responseBody))
+            ->withStatus($statusCode, $statusMessage);
+
     }
 }
