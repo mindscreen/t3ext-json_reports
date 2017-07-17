54dc1c3fdafe5f419083b4e7d637c0c18d7ad69d
heilmann <heilmann@mindscreen.de>
2017-07-17 15:54:37 +0200

    Prevent overwriting project configuration in extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,10 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-// Access to the reports script via HTTP is only granted to the IP addresses or address range defined here
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddresses'] = '';
+// Access to the reports script via HTTP is only granted to the allowed IP addresses or address range
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddresses'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddresses'] = '';
+}
 
 // Register eID Script
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['json_reports'] = \Mindscreen\JsonReports\Controller\ReportsController::class . '::indexAction';
