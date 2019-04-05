0d8d2d5444b4dbb13dcf654d0786369066f606b5
Thomas Heilmann <theilmann@aer.de>
2019-04-05 14:18:17 +0200

    FEATURE: Add report groups that allow to exclude reports from output

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,10 @@
 <?php
 
+use Mindscreen\JsonReports\Command\ReportsCommandController;
+use Mindscreen\JsonReports\Controller\ReportsController;
+use Mindscreen\JsonReports\Output\Json;
+use Mindscreen\JsonReports\Output\Nagios;
+
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
@@ -12,13 +17,17 @@ if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['allowedIpAddr
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output'])) {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output'] = [];
 }
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['json'] = \Mindscreen\JsonReports\Output\Json::class;
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['nagios'] = \Mindscreen\JsonReports\Output\Nagios::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['json'] = Json::class;
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['output']['nagios'] = Nagios::class;
 
+// Define default report group that does not exclude any reports
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['json_reports']['groups']['default'] = [
+    'exclude' => [],
+];
 
 // Register eID Script
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['json_reports'] = \Mindscreen\JsonReports\Controller\ReportsController::class . '::indexAction';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['json_reports'] = ReportsController::class . '::indexAction';
 
 if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = \Mindscreen\JsonReports\Command\ReportsCommandController::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = ReportsCommandController::class;
 }
